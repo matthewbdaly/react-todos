0ee88dec03304b10256c416e82c18c642f46ffdb
Matthew Daly <matthewbdaly@gmail.com>
2015-09-06 18:51:58 +0100

    Initial commit

---
+++ gulpfile.js
@@ -0,0 +1,21 @@
+var gulp = require('gulp');
+var jshint = require('gulp-jshint');
+var source = require('vinyl-source-stream');
+var browserify = require('browserify');
+var reactify = require('reactify');
+
+gulp.task('lint', function () {
+  return gulp.src([
+      'componensts/*.js'
+      ])
+    .pipe(jshint())
+    .pipe(jshint.reporter('jshint-stylish'));
+});
+
+gulp.task('default', function () {
+  return browserify({ entries: ['components/index.jsx'], debug: true })
+    .transform(reactify)
+    .bundle()
+    .pipe(source('index.js'))
+    .pipe(gulp.dest('public/jsx/'));
+});
