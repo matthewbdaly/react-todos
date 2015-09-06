2dc65752e817ae5f8b7889b8c3d4eb1abcb04b52
Matthew Daly <matthewbdaly@gmail.com>
2015-09-06 19:16:27 +0100

    Added unordered list for todos

--- components/list.jsx
+++ components/list.jsx
@@ -1,6 +1,10 @@
 var React = require('react');
 
 React.render(
-  <h1>React Todo</h1>,
+  <div>
+    <h1>React Todo</h1>
+    <ul className="todos">
+    </ul>
+  </div>,
   document.getElementById('view')
 );
