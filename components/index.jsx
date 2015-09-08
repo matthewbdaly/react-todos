2ce2a1dc08547518db4252014eb977dd6362bb32
Matthew Daly <matthewbdaly@gmail.com>
2015-09-08 21:57:41 +0100

    Now seems to be isomorphic

--- components/index.jsx
+++ components/index.jsx
@@ -1,7 +1,9 @@
 var React = require('react');
 var Todos = require('./todos.jsx');
 
+var initialState = JSON.parse(document.getElementById('initial-state').innerHTML);
+
 React.render(
-  <Todos url="/todos" />,
+  <Todos url="/todos" data={initialState} />,
   document.getElementById('view')
 );
